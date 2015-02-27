e9ebb5b79acff6359fd2846b613583efb1e73b06
daisysomus <daisysomus@gmail.com>
2015-02-27 14:16:36 +0800

    initial workspace

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform:ios,  '7.0'
+
+xcodeproj 'TemperatureDemo/TemperatureDemo.xcodeproj'
+pod 'AprilSDK', '~> 1.2.4'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AprilSDK
+#define COCOAPODS_POD_AVAILABLE_AprilSDK
+#define COCOAPODS_VERSION_MAJOR_AprilSDK 1
+#define COCOAPODS_VERSION_MINOR_AprilSDK 2
+#define COCOAPODS_VERSION_PATCH_AprilSDK 4
+
