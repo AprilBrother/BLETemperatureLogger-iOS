3128957d2336ac0beaa75dc501ad35ee9a871919
daisysomus <daisysomus@gmail.com>
2015-02-27 13:52:04 +0800

    initial project

---
+++ TemperatureDemo/TemperatureDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TemperatureDemo
+//
+//  Created by liaojinhua on 15/2/27.
+//  Copyright (c) 2015年 April Brother. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TemperatureDemo/TemperatureDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TemperatureDemo
+//
+//  Created by liaojinhua on 15/2/27.
+//  Copyright (c) 2015年 April Brother. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
